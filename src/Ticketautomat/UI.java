724a98ace7a5741a018baa56d92afa8ac5a66263
Samuel <samuel.meusburger@student.htldornbirn.at>
2024-10-21 10:48:20 +0200

    New version

--- src/Ticketautomat/UI.java
+++ src/Ticketautomat/UI.java
@@ -1,8 +1,33 @@
+
 package Ticketautomat;
 
 public class UI {
-    private boolean buttonPressed;
-    
+    Controller controller;
+    Scanner scanner;
+    Geldausgabe geldausgabe;
 
+    public UI (Controller controller, Scanner scanner, Geldausgabe geldausgabe)
+    {
+        this.controller = controller;
+        this.scanner = scanner;
+        this.geldausgabe = geldausgabe;
+    }
+
+    public void ButtonPressed()
+    {
+        System.out.println("Pressed the Button:     ");
+        controller.CreateTicket();
+    }
+
+    public void InsertMoney(double betrag)
+    {
+        System.out.println(betrag + "€ eingeschmissen");
+        if(betrag >= scanner.getPrice()){
+            scanner.getTicket().setPayedTimeStamp(System.currentTimeMillis());
+            geldausgabe.returnChange(scanner.getPrice()-betrag);
+        } 
+        else{
+            System.out.println("Zu zahlen: " + (scanner.getPrice() - betrag));
+        }
+    }
 }
-
